b7a1955c4cef6bf98b1a0094ddc3431432387b63
Your Name <mambetzumaevesmir@gmail.com>
2024-11-21 09:52:58 +0600

    add

---
+++ app.js
@@ -0,0 +1,12 @@
+let student = {
+    name : 'Esmer',
+    age : 14,
+    hobbies : ['boks','Borba'],
+    mySchool:102,
+    myFathers:['Farhat','Nuraim']     
+}
+
+console.log('My name is', student.name, 'I am', student.age, 'year old', 'My hobbies', student.hobbies, 'I am studing', student.mySchool, 'my father name', 
+    student.myFathers[0], 'my mather name is', student.myFathers[1]
+ );
+
